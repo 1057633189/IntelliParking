75114c8814913a23b7be678f61be487feaa06f4c
Chen Fu <xm1057633189@vip.qq.com>
2020-10-19 17:02:43 +0800

    Add parking lot app
    Add parking lot app

---
+++ App - Driver/app/src/main/java/com/example/IntelliParkingDriver/parkinglot/Reservation.java
@@ -0,0 +1,162 @@
+package com.example.IntelliParkingDriver.parkinglot;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.IntelliParkingDriver.R;
+import com.example.IntelliParkingDriver.util.HttpUtil;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Response;
+
+public class Reservation extends AppCompatActivity {
+
+    private TextView title;
+    private String parkingLotName;
+    private String parkingSpace;
+
+    private TextView beginDate;
+    private TextView beginTime;
+    private TextView endDate;
+    private TextView endTime;
+
+    private int beginYear;
+    private int beginMonth;
+    private int beginDay;
+    private int beginHour;
+    private int beginMinute;
+    private int endYear;
+    private int endMonth;
+    private int endDay;
+    private int endHour;
+    private int endMinute;
+    private String time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+        parkingLotName = getIntent().getStringExtra("extra_data").split("-")[0];
+        parkingSpace = getIntent().getStringExtra("extra_data").split("-")[1];
+
+        title = (TextView) findViewById(R.id.textview_reservation_title);
+        title.setText("Reservation for space \"" + parkingSpace + "\" of \"" + parkingLotName + "\"");
+        beginDate = (TextView) findViewById(R.id.textview_reservation_date_begin);
+        beginTime = (TextView) findViewById(R.id.textview_reservation_time_begin);
+        endDate = (TextView) findViewById(R.id.textview_reservation_date_end);
+        endTime = (TextView) findViewById(R.id.textview_reservation_time_end);
+
+        ((Button) findViewById(R.id.button_reservation_date_begin)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(Reservation.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        beginYear = year;
+                        beginMonth = month + 1;
+                        beginDay = day;
+                        beginDate.setText("Begin date: " + beginYear + "-" + beginMonth + "-" + beginDay);
+                    }
+                }, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        ((Button) findViewById(R.id.button_reservation_time_begin)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new TimePickerDialog(Reservation.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int minute) {
+                        beginHour = hour;
+                        beginMinute = minute;
+                        beginTime.setText("Begin time: " + beginHour + ":" + beginMinute);
+                    }
+                }, Calendar.getInstance().get(Calendar.HOUR_OF_DAY), Calendar.getInstance().get(Calendar.MINUTE), true).show();
+            }
+        });
+
+        ((Button) findViewById(R.id.button_reservation_date_end)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(Reservation.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        endYear = year;
+                        endMonth = month + 1;
+                        endDay = day;
+                        endDate.setText("End date: " + endYear + "-" + endMonth + "-" + endDay);
+                    }
+                }, Calendar.getInstance().get(Calendar.YEAR), Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        ((Button) findViewById(R.id.button_reservation_time_end)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new TimePickerDialog(Reservation.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hour, int minute) {
+                        endHour = hour;
+                        endMinute = minute;
+                        endTime.setText("End time: " + endHour + ":" + endMinute);
+                    }
+                }, Calendar.getInstance().get(Calendar.HOUR_OF_DAY), Calendar.getInstance().get(Calendar.MINUTE), true).show();
+            }
+        });
+
+        Button reservation = (Button) findViewById(R.id.button_reservation);
+        reservation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                time = beginYear + "-" + beginMonth + "-" + beginDay + " " + beginHour + ":" + beginMinute
+                        + "--" + endYear + "-" + endMonth + "-" + endDay + " " + endHour + ":" + endMinute;
+                Log.d("RESERVATION", time);
+                HttpUtil.parkingReservation(parkingLotName, parkingSpace, time, new okhttp3.Callback() {
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        String responseData = Objects.requireNonNull(response.body()).string();
+                        Log.d("RESERVATION", responseData);
+                        if (responseData.equalsIgnoreCase("success")) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(Reservation.this, "Parking reservation successful", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }
+                            });
+                        } else if (responseData.equalsIgnoreCase("error")) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(Reservation.this, "Reservation datetime overlap", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        });
+    }
+
+}
---
+++ App - Parking Lot/app/src/main/java/com/example/IntelliParkingParkingLot/InformationManagement.java
@@ -0,0 +1,148 @@
+package com.example.IntelliParkingParkingLot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Response;
+
+public class InformationManagement extends AppCompatActivity {
+
+    private TextView locationTV;
+    private TextView priceTV;
+    private TextView totalSpaceTV;
+    private TextView availableSpaceTV;
+
+    private Button changePrice;
+
+    private ImageView space1;
+    private ImageView space2;
+    private ImageView space3;
+    private ImageView space4;
+    private ImageView space5;
+
+    private int totalSpace;
+    private int currentSpace;
+
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_management);
+
+        locationTV = (TextView) findViewById(R.id.im_textview_location);
+        priceTV = (TextView) findViewById(R.id.im_textview_price);
+        totalSpaceTV = (TextView) findViewById(R.id.im_textview_total_space);
+        availableSpaceTV = (TextView) findViewById(R.id.im_textview_available_space);
+
+        changePrice = (Button) findViewById(R.id.im_button);
+
+        space1 = (ImageView) findViewById(R.id.im_1);
+        space2 = (ImageView) findViewById(R.id.im_2);
+        space3 = (ImageView) findViewById(R.id.im_3);
+        space4 = (ImageView) findViewById(R.id.im_4);
+        space5 = (ImageView) findViewById(R.id.im_5);
+
+        getParkingLotInfo();
+        handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                getParkingLotInfo();
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.postDelayed(runnable, 1000);
+    }
+
+    private void getParkingLotInfo() {
+        HttpUtil.parkingLotInfo("Hanguang Department Store", new okhttp3.Callback() {
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                currentSpace = 0;
+                String responseData = Objects.requireNonNull(response.body()).string();
+                Log.d("HDS", responseData);
+                try {
+                    JSONObject responseJSON = new JSONObject(responseData);
+                    JSONObject infoJSON = new JSONObject(responseJSON.get("info").toString());
+                    final String location = infoJSON.getString("location");
+                    final String price = infoJSON.getString("price");
+                    totalSpace = Integer.parseInt(infoJSON.getString("total space"));
+                    JSONObject spaceJSON = new JSONObject(responseJSON.get("space").toString());
+                    final String s_1 = spaceJSON.get("1").toString();
+                    final String s_2 = spaceJSON.get("2").toString();
+                    final String s_3 = spaceJSON.get("3").toString();
+                    final String s_4 = spaceJSON.get("4").toString();
+                    final String s_5 = spaceJSON.get("5").toString();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (s_1.equalsIgnoreCase("using")) {
+                                space1.setImageResource(R.drawable.using1);
+                            } else if (s_1.equalsIgnoreCase("empty")) {
+                                currentSpace += 1;
+                                space1.setImageResource(R.drawable.empty1);
+                            }
+                            if (s_2.equalsIgnoreCase("using")) {
+                                space2.setImageResource(R.drawable.using2);
+                            } else if (s_2.equalsIgnoreCase("empty")) {
+                                currentSpace += 1;
+                                space2.setImageResource(R.drawable.empty2);
+                            }
+                            if (s_3.equalsIgnoreCase("using")) {
+                                space3.setImageResource(R.drawable.using3);
+                            } else if (s_3.equalsIgnoreCase("empty")) {
+                                currentSpace += 1;
+                                space3.setImageResource(R.drawable.empty3);
+                            }
+                            if (s_4.equalsIgnoreCase("using")) {
+                                space4.setImageResource(R.drawable.using4);
+                            } else if (s_4.equalsIgnoreCase("empty")) {
+                                currentSpace += 1;
+                                space4.setImageResource(R.drawable.empty4);
+                            }
+                            if (s_5.equalsIgnoreCase("using")) {
+                                space5.setImageResource(R.drawable.using5);
+                            } else if (s_5.equalsIgnoreCase("empty")) {
+                                currentSpace += 1;
+                                space5.setImageResource(R.drawable.empty5);
+                            }
+                            locationTV.setText("Location: " + location);
+                            priceTV.setText("Price: " + price);
+                            totalSpaceTV.setText("Total parking space: " + totalSpace);
+                            availableSpaceTV.setText("Available parking space: " + currentSpace);
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+
+}
